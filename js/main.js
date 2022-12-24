6427c5d9c31c8016521e45f70866ef30a848fce4
ibrag1mov <ibragimov.abdulhay@mail.ru>
2022-12-24 22:07:39 +0500

    edited

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
-const elModalBtn=document.querySelector('.js-add');
+const elAddBtn=document.querySelector('.js-add');
+const elClearBtn=document.querySelector('.js-clear');
 const elModal=document.querySelector('#modal');
 const elEditModal=document.querySelector('#edit');
 const elCard=document.querySelector('.card');
 const elListTitile = document.querySelector('.js-list-title')
 
-elModalBtn.addEventListener('click',(evt)=>{
+
+// add btn
+elAddBtn.addEventListener('click',(evt)=>{
    if(elModal.classList.contains('d-none')){
     elModal.setAttribute('class', 'modal-wrapper d-flex')
    }
-   
 })
 
 elModal.addEventListener('click', (evt)=>{
@@ -21,6 +23,7 @@ elModal.addEventListener('click', (evt)=>{
 
 
 
+// loading
 var loader=document.querySelector('.load');
 
 window.addEventListener("load", ()=>{
@@ -28,16 +31,25 @@ window.addEventListener("load", ()=>{
 })
 
 // contact list
+const elList=document.querySelector('.js-list');
+
 
 const elForm=document.querySelector('.js-form');
-const elEditForm=document.querySelector('.js-edit-form');
-const elList=document.querySelector('.js-list');
 const elNameInput = document.querySelector('#name-input');
+const elRelationshipSelect = document.querySelector('#relationship-select');
 const elRelationshipInput = document.querySelector('#relationship-input');
-const elPhoneInput = document.querySelector('#phone-input');
-const elEditNameInput = document.querySelector('#name-edit-input');
+const elPhoneCode = document.querySelector('#phone-code');
+const elPhoneNumber = document.querySelector('#phone-number');
+
+// edit
+
+const elEditForm=document.querySelector('.js-edit-form');
+const elEditNameInput=document.querySelector('#name-edit-input')
+const elEtidRelationshipSelect = document.querySelector('#relationship-edit-select');
 const elEditRelationshipInput = document.querySelector('#relationship-edit-input');
-const elEditPhoneInput = document.querySelector('#phone-edit-input');
+const elEditPhoneCode = document.querySelector('#phone-edit-code');
+const elEditPhoneNumber = document.querySelector('#phone-edt-number');
+
 
 const contact=[];
 
@@ -52,32 +64,36 @@ const renderContact = (arr, node)=>{
       const newDivBtn = document.createElement('div');
       const newName = document.createElement('span');
       const newRelationship = document.createElement('span');
+      const newPhoneLink = document.createElement('a');
       const newPhone = document.createElement('span');
       const newEditBtn=document.createElement('button');
       const newDeleteBtn=document.createElement('button');
    
       newItem.setAttribute('class', 'd-flex w-100 p-0 list-group-item align-items-center js-item ');
-      newDivN.setAttribute('class', 'new-w-30 ps-3 rounded-start py-3');
-      newDivR.setAttribute('class', 'new-w-31 border-2 border-start border-end ps-3  py-3');
-      newDivP.setAttribute('class', 'new-w-30 ps-3 py-3');
+      newDivN.setAttribute('class', 'new-w-30 ps-3 rounded-start border-2  border-end py-3');
+      newDivR.setAttribute('class', 'new-w-30  ps-3  py-3');
+      newDivP.setAttribute('class', 'new-w-31 ps-3 py-3 border-2   border-start');
       newDivBtn.setAttribute('class', 'new-w-10 ps-3 d-flex');
       newName.setAttribute('class', 'fs-5');
       newRelationship.setAttribute('class', ' fs-5');
-      newPhone.setAttribute('class', 'phone fs-5');
+      newPhoneLink.setAttribute('class', 'phone-link')
+      newPhone.setAttribute('class', 'phone fs-5 pe-none');
       newEditBtn.setAttribute('class', 'btn bg-light bg-gradient me-2 shadow js-edit-btn');
       newDeleteBtn.setAttribute('class', 'btn bg-danger text-white bg-gradient shadow js-delete-btn');
    
       newName.textContent = item.name;
       newRelationship.textContent = item.relationship;
-      newPhone.textContent = item.phone;
+      newPhone.textContent =`+998${item.code}${item.number}`;
       newEditBtn.innerHTML = `<i class="bi bi-pen  pe-none"></i>`;
       newDeleteBtn.innerHTML = `<i class="bi bi-x-lg pe-none"></i>`;
       newEditBtn.dataset.contactId = item.id;
       newDeleteBtn.dataset.contactId = item.id;
-
+      newPhoneLink.href=`tel:998${item.code}${item.number}`
+      
+      newPhoneLink.appendChild(newPhone);
       newDivN.appendChild(newName);
       newDivR.appendChild(newRelationship);
-      newDivP.appendChild(newPhone);
+      newDivP.appendChild(newPhoneLink);
       newDivBtn.appendChild(newEditBtn);
       newDivBtn.appendChild(newDeleteBtn);
       newItem.appendChild(newDivN);
@@ -89,36 +105,135 @@ const renderContact = (arr, node)=>{
       node.appendChild(newItem);
    });
 
+   if(arr.length !== 0){
+      if(elListTitile.classList.contains('d-none')){
+         elListTitile.classList.remove('d-none');
+         elClearBtn.classList.remove('d-none');
+         elAddBtn.setAttribute('class','btn btn-success w-25 shadow p-2 rounded js-add')
+      }
+   }
+   if(arr.length === 0){
+      if(!elListTitile.classList.contains('d-none')){
+         elListTitile.classList.add('d-none');
+         elClearBtn.classList.add('d-none');
+         elAddBtn.setAttribute('class','btn mx-auto btn-success w-50 shadow p-2 rounded js-add')
+         const elText = document.createElement('p');
+         elText.textContent = 'Add a contact 😐';
+         elText.setAttribute('class', 'text-center text-white fs-4 mt-5 pt-5');
+         elList.appendChild(elText)
+      }
+   }
 
 }
+
 
 if(contact.length){
    renderContact(contact, elList);
 }
 else{
    const elText = document.createElement('p');
-   elText.textContent = 'Contact qo\'shing';
+   elText.textContent = 'Add a contact 😐';
    elText.setAttribute('class', 'text-center text-white fs-4 mt-5 pt-5');
    elList.appendChild(elText)
 }
 
+elRelationshipSelect.addEventListener('change', (evt)=>{
+   if(elRelationshipSelect.value=="Another"){
+      elRelationshipInput.classList.remove('d-none')
+      elPhoneCode.setAttribute('class',' form-control new-w-10  mb-3 shadow outline-blue');
+      elPhoneNumber.setAttribute('class',' form-control new-w-60  mb-3 shadow outline-blue');
+   }
+   if(elRelationshipSelect.value!="Another"){
+      elRelationshipInput.classList.add('d-none')
+      elPhoneCode.setAttribute('class',' form-control new-w-10  mb-5 shadow outline-blue');
+      elPhoneNumber.setAttribute('class',' form-control new-w-60  mb-5 shadow outline-blue');
+   }
+   elRelationshipSelect.setAttribute('class', 'form-select new-w-60 mx-auto mb-2 shadow outline-blue')
+
+})
+
+elPhoneCode.addEventListener('input', (evt)=>{
+   if(elPhoneCode.value.length ){
+      elPhoneCode.setAttribute('class',' form-control new-w-10  mb-5 shadow outline-blue')
+   }
+})
+
+elPhoneNumber.addEventListener('input', (evt)=>{
+   if(elPhoneNumber.value.length ){
+      elPhoneNumber.setAttribute('class',' form-control new-w-60  mb-5 shadow outline-blue')
+   }
+})
+
+elRelationshipInput.addEventListener('input', (evt)=>{
+   if(elRelationshipInput.value.length){
+      elRelationshipInput.setAttribute('class','form-control new-w-60 mx-auto mb-2 shadow outline-blue')
+   }
+})
+
+elNameInput.addEventListener('input', (evt)=>{
+   if(elNameInput.value.length){
+      elNameInput.setAttribute('class','form-control new-w-60 mx-auto mb-2 shadow outline-blue')
+   }
+})
+
+
+let truth=true;
 
 elForm.addEventListener('submit', (evt)=>{
-   evt.preventDefault();
+   
+   if(elNameInput.value.length === 0){
+      elNameInput.setAttribute('class','form-control new-w-60 mx-auto mb-2 shadow outline-red border-danger')
+   }
 
-   if(elNameInput.value != '' && elRelationshipInput != '' && elPhoneInput.value.length>12 && elPhoneInput.value.length<14){
+   if(elRelationshipSelect.value == 'Relationship'){
+      elRelationshipSelect.setAttribute('class', 'form-select new-w-60 mx-auto mb-2 shadow outline-red')
+
+   }
+
+   if(elRelationshipSelect.value=="Another"){
+      if(elRelationshipInput.value.length !==0){
+         truth=true;
+         
+      }
+      if(elRelationshipInput.value.length ===0){
+         truth=false;
+         elRelationshipInput.setAttribute('class','form-control new-w-60 mx-auto mb-2 shadow border-5 outline-red border-danger')
+      }
+      
+   }
+
+   if(elPhoneCode.value.length!==2){
+       elPhoneCode.setAttribute('class',' form-control new-w-10  mb-5 shadow border-5 rounded-start outline-red border-danger')
+   }
+
+   if(elPhoneNumber.value.length!==7){
+      elPhoneNumber.setAttribute('class','form-control new-w-60  mb-5 shadow border-5 rounded-end outline-red border-danger')
+   }
+
+
+   if(elNameInput.value.length === 0 || elRelationshipSelect.value == 'Relationship' || truth!=true || elPhoneCode.value.length!==2 || elPhoneNumber.value.length!==7){
+      alert("Enter complete Contact information…❗")
+   }
+
+   if(elNameInput.value != '' && elRelationshipSelect.value != 'Relationship' && elPhoneCode.value.length===2 && elPhoneNumber.value.length===7 && truth!=false){
        let elNameValue = elNameInput.value;
-       let elRelationshipValue = elRelationshipInput.value;
-       let elPhoneValue = elPhoneInput.value;
+       let elRelationshipSelectValue = elRelationshipSelect.value;
+       let elRelationshipInputValue = elRelationshipInput.value;
+       let elPhoneCodeValue = elPhoneCode.value;
+       let elPhoneNumberValue = elPhoneNumber.value;
+       elRelationshipSelect.value=="Relationship";
+       elRelationshipInput.value='';
        elNameInput.value='';
        elRelationshipInput.value='';
-       elPhoneInput.value="+998";
+       elPhoneCode.value="";
+       elPhoneNumber.value='';
    
        const newContact = {
            id: contact.length > 0 ? contact[contact.length-1].id + 1 : 1,
            name: elNameValue,
-           relationship: elRelationshipValue,
-           phone: elPhoneValue
+           relationship: elRelationshipSelectValue == "Another" ? elRelationshipInputValue : elRelationshipSelectValue,
+           code: elPhoneCodeValue,
+           number: elPhoneNumberValue
        };
 
        contact.push(newContact);
@@ -127,57 +242,16 @@ elForm.addEventListener('submit', (evt)=>{
 
        elListTitile.setAttribute('class', 'list d-flex list-group list-group-horizontal mx-auto w-100 p-0 js-list-title')
        elModal.setAttribute('class', 'modal-wrapper d-none')
-   };
-   if(elPhoneInput.value.length<=12 || elPhoneInput.value.length>13){
-      elPhoneInput.setAttribute('class', 'form-control new-w-60 mx-auto mb-5 shadow text-danger border-danger border-3')
    }
-   else if(elPhoneInput.value.length=13){
-      elPhoneInput.setAttribute('class', 'form-control new-w-60 mx-auto mb-5 shadow text-dark border-primary border-3')
-   }
-});
-
-
-elEditForm.addEventListener('submit', (evt)=>{
-   elList.innerHTML=''
    evt.preventDefault();
-   if(elEditNameInput.value != '' && elEditRelationshipInput != '' && elEditPhoneInput.value.length>12 && elEditPhoneInput.value.length<14){
-       let elEditNameValue = elEditNameInput.value;
-       let elRditRelationshipValue = elEditRelationshipInput.value;
-       let elRditPhoneValue = elEditPhoneInput.value;
-
-   
-       const newContact = {
-           id: contact.length > 0 ? contact[contact.length-1].id + 1 : 1,
-           name: elEditNameValue,
-           relationship: elRditRelationshipValue,
-           phone: elRditPhoneValue
-       };
-
-       contact.push(newContact);
-
-       renderContact(contact, elList);
-
-      
-       elEditModal.setAttribute('class', 'modal-wrapper d-none')
-   };
-   if(elEditPhoneInput.value.length<=12 || elEditPhoneInput.value.length>13){
-      elEditPhoneInput.setAttribute('class', 'form-control new-w-60 mx-auto mb-5 shadow text-danger border-danger border-3')
-   }
-   else if(elEditPhoneInput.value.length=13){
-      elEditPhoneInput.setAttribute('class', 'form-control new-w-60 mx-auto mb-5 shadow text-dark border-primary border-3')
-   }
-});
-
-elPhoneInput.addEventListener('keypress', (evt)=>{
-
-      elPhoneInput.setAttribute('class', 'form-control new-w-60 mx-auto mb-5 shadow text-dark border-primary border-3')
-   
 })
-elEditPhoneInput.addEventListener('keypress', (evt)=>{
 
-      elEditPhoneInput.setAttribute('class', 'form-control new-w-60 mx-auto mb-5 shadow text-dark border-primary border-3')
-   
-})
+const editFunction=(array)=>{
+
+   console.log(array);
+
+}
+let newId=0;
 
 elList.addEventListener('click', (evt)=>{
    if(evt.target.matches('.js-delete-btn')){
@@ -191,24 +265,25 @@ elList.addEventListener('click', (evt)=>{
 
    }
    if(evt.target.matches('.js-edit-btn')){
+
       const contactId=evt.target.dataset.contactId;
 
       const findedItem = contact.find((item)=> item.id == contactId);
+      const findedIndex = contact.findIndex((item)=> item.id == contactId);
 
+
+      newId=findedIndex;
+    
+      elEditNameInput.value=findedItem.name;
+      elEditRelationshipInput.value=findedItem.relationship;
+      elEditPhoneCode.value =findedItem.code;
+      elEditPhoneNumber.value = findedItem.number;
       
-      elEditNameInput.value = findedItem.name;
-      elEditRelationshipInput.value = findedItem.relationship;
-      elEditPhoneInput.value = findedItem.phone;
 
-      // findedItem.name = elEditNameInput.value;
-      // findedItem.relationship = elEditRelationshipInput.value;
-      // findedItem.phone = elEditPhoneInput.value;
 
       if(elEditModal.classList.contains('d-none')){
          elEditModal.setAttribute('class', 'edit-modal-wrapper d-flex')
       }
-
-      // renderContact(contact, elList)
    }
 })
       
@@ -217,3 +292,84 @@ elEditModal.addEventListener('click',(evt)=>{
       elEditModal.setAttribute('class', 'edit-modal-wrapper d-none')
  }
 })
+
+
+
+elEditPhoneCode.addEventListener('input', (evt)=>{
+   if(elEditPhoneCode.value.length ){
+      elEditPhoneCode.setAttribute('class',' form-control new-w-10  mb-5 shadow outline-blue')
+   }
+})
+
+elEditPhoneNumber.addEventListener('input', (evt)=>{
+   if(elEditPhoneNumber.value.length ){
+      elEditPhoneNumber.setAttribute('class',' form-control new-w-60  mb-5 shadow outline-blue')
+   }
+})
+
+elEditRelationshipInput.addEventListener('input', (evt)=>{
+   if(elEditRelationshipInput.value.length){
+      elEditRelationshipInput.setAttribute('class','form-control new-w-60 mx-auto mb-2 shadow  outline-blue')
+   }
+})
+
+elEditNameInput.addEventListener('input', (evt)=>{
+   if(elEditNameInput.value.length){
+      elEditNameInput.setAttribute('class','form-control new-w-60 mx-auto mb-2 shadow outline-blue')
+   }
+})
+
+
+
+elEditForm.addEventListener('submit', (evt)=>{
+   evt.preventDefault();
+   if(elEditNameInput.value.length === 0){
+      elEditNameInput.setAttribute('class','form-control new-w-60 mx-auto mb-2 shadow outline-red border-danger')
+   }
+
+   if(elEditRelationshipInput.value.length === 0){
+      elEditRelationshipInput.setAttribute('class','form-control new-w-60 mx-auto mb-2 shadow  outline-red')
+   }
+
+
+   if(elEditPhoneCode.value.length!==2){
+       elEditPhoneCode.setAttribute('class','  form-control new-w-10  mb-5 shadow  border-5 rounded-start outline-red border-danger')
+   }
+
+   if(elEditPhoneNumber.value.length!==7){
+      elEditPhoneNumber.setAttribute('class','form-control new-w-60  mb-5 shadow border-5 rounded-end outline-red border-danger')
+   }
+
+
+   if(elEditNameInput.value.length === 0 || elEditRelationshipInput.value.length===0 || elEditPhoneCode.value.length!==2 || elEditPhoneNumber.value.length!==7){
+      alert("Enter complete Contact information…❗")
+   }
+
+   if(elEditNameInput.value.length !== 0 && elEditRelationshipInput.value.length!==0 && elEditPhoneCode.value.length===2 && elEditPhoneNumber.value.length===7 ){
+
+      contact[newId].name=elEditNameInput.value;
+      contact[newId].relationship=elEditRelationshipInput.value;
+      contact[newId].code=elEditPhoneCode.value;
+      contact[newId].number=elEditPhoneNumber.value;
+
+      renderContact(contact, elList);
+
+
+      elEditModal.setAttribute('class', 'edit-modal-wrapper d-none')
+      
+   }
+
+
+})
+
+
+// clear btn
+
+elClearBtn.addEventListener('click', (evt)=>{
+   contact.splice(0);
+   renderContact(contact, elList);
+   elListTitile.classList.add('d-none');
+   elClearBtn.classList.add('d-none');
+   elAddBtn.setAttribute('class','btn mx-auto btn-success w-50 shadow p-2 rounded js-add')
+})
+
